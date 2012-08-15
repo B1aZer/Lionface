d37fb1263b0d6422885065649dee8f99028a9b38
Ubuntu <ubuntu@domU-12-31-39-06-CC-AF.compute-1.internal>
2012-08-15 17:20:33 +0000

    serer test

--- settings.py
+++ settings.py
@@ -169,7 +169,7 @@ cwd = dirname(abspath(__file__))
 
 sys.path.insert(0, join(cwd, "apps"))
 
-sys.path.append('Users/blaze/Sites/clients/Nick/lionface/')
+sys.path.append('/var/www/placeless/lionface/whispering-anchorage-2296/')
 
 os.environ["CELERY_LOADER"] = "django"
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'    
