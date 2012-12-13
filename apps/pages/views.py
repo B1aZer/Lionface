78b6f174de9d00181c6c63d70c963693859baa7d
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2012-12-13 20:03:44 +0400

    Ecommerce fixes 5

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -678,8 +678,7 @@ def settings(request, slug=None):
     friday = now.date() + dateclass.timedelta(4 - now.weekday())
     closing = dateclass.datetime(friday.year, friday.month, friday.day, 17, 00, tzinfo=pytz.timezone('US/Eastern'))
     if (mint > 19 and mint < 30) or (mint > 49 and mint <= 59):
-        # TODO
-        show_bids = True
+        show_bids = False
     else:
         show_bids = True
 
