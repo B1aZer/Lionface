ad7725e3f23de6763b6537ae5681b57f9315595e
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2012-12-19 13:03:13 +0400

    morning fixes

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -3,10 +3,10 @@ from .models import Pages
 
 class PagesAdmin(admin.ModelAdmin):
     list_display = ('name', 'username', 'user', 'loves_limit', 'featured', 'exempt')
-    fields = ['name', 'loves_limit', 'featured', 'exempt']
+    fields = ['name', 'loves_limit', 'exempt']
     search_fields = ['name', 'username']
     actions = None
-    list_editable = ['featured', 'exempt']
+    list_editable = ['exempt']
 
 admin.site.register(Pages, PagesAdmin)
 
