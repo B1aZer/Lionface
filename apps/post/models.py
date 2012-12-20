900260e815b236d1eb65dc347419a9e6f4b4dee3
Dmitry on Server <dmitry.branitskiy@gmail.com>
2012-12-20 12:40:57 +0400

    server templates

--- apps/post/models.py
+++ apps/post/models.py
@@ -419,7 +419,7 @@ class PageSharePost(PagePost):
 
     def render(self):
         #import pdb;pdb.set_trace()
-        return mark_safe("""<a href='%s'>%s</a> <span style='color: #AAA;'>shared a post from</span> <a href='%s'>%s</a>
+        return mark_safe("""<a href='' style='font-size: 14pt;'>Page Name</a> <a href='%s' class='admin_name'>%s</a> <span style='color: #AAA;'>shared a post from</span> <a href='%s'>%s</a>
                             <div class='share_content'>%s</div>""" % (self.user_to.get_absolute_url(), self.user_to.get_full_name(), self.user.get_absolute_url(), self.user.get_full_name(), self.content))
 
 
