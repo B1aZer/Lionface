5a1f175561d0f57bbf44b99a0d196fcf58617622
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2012-12-21 19:56:34 +0400

    search 1.

--- apps/search/forms.py
+++ apps/search/forms.py
@@ -10,9 +10,15 @@ class SearchForm(ModelSearchForm):
 
     def search(self):
         def sorting_by_degree(result):
-            if isinstance(result.object,Pages):
+            if result.model is Pages:
                 return '--'
-            return result.object.get_degree_for(user)
+            try:
+                v = result.object.get_degree_for(user)
+            except:
+                # this could happen
+                # run rebuild_index
+                v = '--'
+            return v
         #res = super(SearchForm, self).search()
         #res = SearchQuerySet().filter(username_auto=self.cleaned_data['q'])
         if len(self.cleaned_data['q'].strip().split(" ")) > 1:
