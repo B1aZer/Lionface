4703f0654b34ffa85b927cc0b7986192de66d517
Alex Po <alex-86p@yandex.ru>
2012-12-28 22:37:28 +0400

    comment func in public.js

--- static/js/public.js
+++ static/js/public.js
@@ -1,8 +1,10 @@
+/*
 function ReverseContentDisplay(d) {
     if(d.length < 1) { return; }
     if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
     else { document.getElementById(d).style.display = "none"; }
 }
+*/
 
 $(document).ready(function() {
 
