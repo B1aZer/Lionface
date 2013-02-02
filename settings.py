7e3ef590de228d39d7fe23f2ff231ed062f17ac2
Dmitry on Server <dmitry.branitskiy@gmail.com>
2013-02-02 20:51:16 +0400

    server templates

--- settings.py
+++ settings.py
@@ -325,6 +325,8 @@ SITE_ID = 1
 #BROKER_URL = "django://"
 
 ACCOUNT_ACTIVATION_DAYS = 7
+# Number of seconds of inactivity before a user is marked offline
+USER_ONLINE_TIMEOUT = 300
 
 if os.environ.get('production'):
     try:
